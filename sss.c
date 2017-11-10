8e16aa6a6b50fb5412861a001cd697aa2bd0a4fe
20171124610012 <33539174+20171124610012@users.noreply.github.com>
2017-11-10 12:11:40 +0530

    Add files via upload

---
+++ sss.c
@@ -0,0 +1,25 @@
+#include<stdio.h> 
+void main()
+ {
+   float a;
+ float surface_area,volume;
+
+  printf("enter size of any side of a cube:");
+
+  scanf("%f",&a);
+  
+  surface_area=6*(a*a);
+  volume=a*a*a;
+
+  printf("surface area of cube is: %.3f",surface_area);
+  printf("\nvolme of cube is:%.3f",volume);
+  
+ } 
+  
+ 	 
+ enter size of any side of a cube:4
+surface area of cube is: 96.000
+volme of cube is:64.000dell@dell-OptiPlex-7040:~$ 
+
+  	
+  
